7a2dad2980801ad0d93e65411e23cc36ffda47b8
Kevin David Bedoya Garcia <kevi0518@gmail.com>
2021-10-17 05:45:09 -0500

    Sopa de letras terminada

--- reto-1_frontend-main/menu.js
+++ reto-1_frontend-main/menu.js
@@ -19,22 +19,47 @@
         subMenuBtn.forEach(element =>
             {
                 element.addEventListener('click', ()=>{
-                    let subMenu = element.children[1];
-                    let heightSubMenu = 0;
-                    element.classList.toggle('menu--active')
-                    console.log(element);
-                    let heightMenuV = menu.clientHeight;
-                    if(subMenu.clientHeight === 0){
-                        heightSubMenu = subMenu.scrollHeight;
-                        heightMenuV = (heightMenuV+heightSubMenu);
-                    }else{
-                        heightMenuV = heightMenuV - subMenu.clientHeight;
+                    if(window.innerWidth < 600){
+                        let subMenu = element.children[1];
+                        let heightSubMenu = 0;
+                        element.classList.toggle('menu--active')
+                        let heightMenuV = menu.clientHeight;
+                        if(subMenu.clientHeight === 0){
+                            heightSubMenu = subMenu.scrollHeight;
+                            heightMenuV = (heightMenuV+heightSubMenu);
+                        }else{
+                            heightMenuV = heightMenuV - subMenu.clientHeight - 60;
+                        }
+                        subMenu.style.height = `${heightSubMenu}px`;
+                        menu.style.height = heightMenuV + 'px';
                     }
-                    subMenu.style.height = `${heightSubMenu}px`;
-                    menu.style.height = heightMenuV + 'px';
                 })
             })
     }
 
-    addClick();
+    const restart = ()=>{
+        subMenuBtn.forEach(element =>
+        {
+            if(element.children[1].getAttribute('style')){
+                element.children[1].removeAttribute('style');
+                element.classList.remove('menu--active');
+            }
+        })
+    }
+    
+    if(window.innerWidth <= 600){
+        addClick();
+    }
+
+    window.addEventListener('resize', ()=>{
+        if(menu.classList.contains("mostrar")){
+            menu.classList.remove("mostrar");
+            menu.removeAttribute("style");
+        }
+        if(window.innerWidth > 600){
+            restart();
+        }else{
+            addClick();
+        }
+    });
 })();
